783ff1feaf2845cb8560d7583af9054e320dd0f6
lmagitem <lucmagitem@gmail.com>
2020-08-14 11:41:05 +0200

    Login with Cognito

--- src/main/webapp/app/core/login/login.service.ts
+++ src/main/webapp/app/core/login/login.service.ts
@@ -3,14 +3,16 @@ import { Location } from '@angular/common';
 
 import { AuthServerProvider } from 'app/core/auth/auth-session.service';
 import { Logout } from './logout.model';
+import { InfoResponse, ProfileInfo } from 'app/layouts/profiles/profile-info.model';
+import { map, shareReplay, filter, first } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+import { ProfileService } from 'app/layouts/profiles/profile.service';
 
 @Injectable({ providedIn: 'root' })
 export class LoginService {
   constructor(private location: Location, private authServerProvider: AuthServerProvider) {}
 
   login(): void {
-    // If you have configured multiple OIDC providers, then, you can update this URL to /login.
-    // It will show a Spring Security generated login page with links to configured OIDC providers.
     location.href = `${location.origin}${this.location.prepareExternalUrl('oauth2/authorization/oidc')}`;
   }
 
